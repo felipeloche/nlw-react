1a3990c8f3ffdcf379387cab5776e1934f35eb39
felipeloche <felipe.loche@icloud.com>
2025-02-18 23:52:51 -0300

    primeiro commit

---
+++ web/src/app/invite/invite-link-input.tsx
@@ -0,0 +1,19 @@
+import { IconButton } from "@/components/icon-button";
+import { InputRoot, InputIcon, InputField } from "@/components/input";
+import { Link, Copy } from "lucide-react";
+
+export function InviteLinkInput () {
+    return(
+        <InputRoot>
+            <InputIcon>
+                <Link className='size-5'/>
+            </InputIcon>
+
+            <InputField readOnly defaultValue="http://localhost:3000/invite/adasdasr323"/>
+
+            <IconButton className='-mr-2'>
+                <Copy className='size-5' />
+            </IconButton>
+        </InputRoot>
+    )
+}
