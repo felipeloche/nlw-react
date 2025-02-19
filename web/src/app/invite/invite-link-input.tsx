ff569912318b6f9b4c2a266e36532d005fda5d84
Felipe C. B. Loche <166451828+felipeloche@users.noreply.github.com>
2025-02-19 18:10:10 -0300

    feat: add copy button and form validation (#1)

--- web/src/app/invite/invite-link-input.tsx
+++ web/src/app/invite/invite-link-input.tsx
@@ -1,17 +1,27 @@
+'use client'
+
 import { IconButton } from "@/components/icon-button";
 import { InputRoot, InputIcon, InputField } from "@/components/input";
 import { Link, Copy } from "lucide-react";
 
-export function InviteLinkInput () {
+interface InviteLinkInputProps {
+    inviteLink: string
+}
+
+export function InviteLinkInput ({ inviteLink}: InviteLinkInputProps) {
+    function copyInviteLink(){
+        navigator.clipboard.writeText(inviteLink)
+    }
+    
     return(
         <InputRoot>
             <InputIcon>
                 <Link className='size-5'/>
             </InputIcon>
 
-            <InputField readOnly defaultValue="http://localhost:3000/invite/adasdasr323"/>
+            <InputField readOnly defaultValue={inviteLink}/>
 
-            <IconButton className='-mr-2'>
+            <IconButton className='-mr-2' onClick={copyInviteLink}>
                 <Copy className='size-5' />
             </IconButton>
         </InputRoot>
